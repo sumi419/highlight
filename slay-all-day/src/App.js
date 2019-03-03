72d00ba33b22bf24fca6d2cd11ba4c9b7a3a8e96
sumi419 <sasgar04@gmail.com>
2019-03-03 11:49:22 -0800

    added axios and state

--- slay-all-day/src/App.js
+++ slay-all-day/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+   componentDidMount() {
+     const api = 'http://makeup-api.herokuapp.com/api/v1/';
+     axios
+       .get(`${api}products.json`)
+       .then((res) => {
+         console.log(res);
+         return res.json();
+       })
+       .then(data => {
+        this.setState({ products: [...this.state.products, ...data.results] });
+      })
+       .catch((err) => console.error(err));
+   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div> Hello World </div>
       </div>
     );
   }
