5bf766aa9394ebda7a2fe80c786d00171c350161
sumi419 <sasgar04@gmail.com>
2019-03-03 22:44:35 -0800

    Nav Bar of product types 💄

--- slay-all-day/src/components/NavBar.jsx
+++ slay-all-day/src/components/NavBar.jsx
@@ -4,29 +4,17 @@ import './NavBar.css';
 const NavBar = () => {
 	return (
 		<div className='nav-container'>
-			<h1> Slay ✨</h1>
+			<h1> Slay All Day ✨</h1>
 			<div className='main-nav'>
-				<div>
-					Face 😊
+				<div className='product-type'>
+					<div>Foundation</div>
 					<div>Blush</div>
 					<div>Bronzer</div>
-					<div>Foundation</div>
-				</div>
-				<div>
-					Eyes 😍
 					<div>Eyebrow</div>
-					<div>Eyeliner</div>
 					<div>Eyeshadow</div>
+					<div>Eyeliner</div>
 					<div>Mascara</div>
-				</div>
-				<div>
-					Lips 👄
-					<div>Lip Liner</div>
-					<div>Lipstick 💄💋</div>
-				</div>
-				<div>
-					Nails 💅
-					<div>Nail Polish</div>
+					<div>Lipstick</div>
 				</div>
 			</div>
 		</div>
@@ -34,11 +22,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-// create Nav Bar
-// have brand, product type
-// for individual products so price? tag, name, image, product link on image, add comment section
-
-// only show products that have images
-// only show items that have prices more than 0$
-// limit amount of items shown in initial load front page
