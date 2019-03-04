e5acd36af6e3ae3c414ac5f647a42247927186ff
sumi419 <sasgar04@gmail.com>
2019-03-03 16:44:43 -0800

    got rid of constructor

--- slay-all-day/src/App.js
+++ slay-all-day/src/App.js
@@ -4,32 +4,29 @@ import axios from 'axios';
 import ProductsList from './components/ProductsList';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: []
-    };
-  }
-   componentDidMount() {
-     const api = 'http://makeup-api.herokuapp.com/api/v1/';
-     axios
-       .get(`${api}products.json`)
-       .then(res => {
-        console.log(res.data)
-        this.setState({ products: res.data })
-        // this.setState({ products: [...this.state.products, ...data.results] });
-      })
-       .catch((err) => console.error(err));
-   }
+	state = {
+		products: []
+	};
+	componentDidMount() {
+		const api = 'http://makeup-api.herokuapp.com/api/v1/';
+		axios
+			.get(`${api}products.json`)
+			.then((res) => {
+				console.log(res.data);
+				this.setState({ products: res.data });
+				// this.setState({ products: [...this.state.products, ...data.results] });
+			})
+			.catch((err) => console.error(err));
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <div> Hello World </div>
-        <ProductsList products={this.state.products}/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<div> Hello World </div>
+				<ProductsList products={this.state.products} />
+			</div>
+		);
+	}
 }
 
 export default App;
