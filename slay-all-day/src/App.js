462d226ab0795f78cd3e8ad169357f4b74ec81ee
sumi419 <sasgar04@gmail.com>
2019-03-04 21:39:26 -0800

    Eyeshadows added 💖✨

--- slay-all-day/src/App.js
+++ slay-all-day/src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Foundation from './components/Foundation';
 import Blush from './components/Blush';
+import Bronzer from './components/Bronzer';
 import { Route, withRouter } from 'react-router-dom';
 import Home from './components/Home';
+import Eyebrows from './components/Eyebrows';
+import Eyeshadow from './components/Eyeshadow';
 
 class App extends Component {
 	render() {
@@ -14,6 +17,9 @@ class App extends Component {
 				<Route exact path='/' component={Home} />
 				<Route exact path='/foundation' component={Foundation} />
 				<Route exact path='/blush' component={Blush} />
+				<Route exact path='/bronzer' component={Bronzer} />
+				<Route exact path='/eyebrows' component={Eyebrows} />
+				<Route exact path='/eyeshadow' component={Eyeshadow} />
 			</div>
 		);
 	}
