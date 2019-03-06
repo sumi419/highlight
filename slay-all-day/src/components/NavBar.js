cce578bc3dd8cedbf3328cef23223315c7bf6cab
sumi419 <sasgar04@gmail.com>
2019-03-05 18:20:11 -0800

    added icons for each product 💄💋🎀

--- slay-all-day/src/components/NavBar.js
+++ slay-all-day/src/components/NavBar.js
@@ -11,27 +11,59 @@ const NavBar = () => {
 			<div className='main-nav'>
 				<div className='product-type'>
 					<Link to='/foundation'>
+						<img
+							src='https://cdn1.iconfinder.com/data/icons/cosmetic-and-beauty-flat/64/cosmetic-27-512.png'
+							alt='icon'
+						/>
 						<div>Foundation</div>
 					</Link>
 					<Link to='/blush'>
+						<img
+							src='https://cdn1.iconfinder.com/data/icons/shopping-hand-drawn-1/128/38-512.png'
+							alt='icon'
+						/>
 						<div>Blush</div>
 					</Link>
 					<Link to='/bronzer'>
+						<img
+							src='https://cdn4.iconfinder.com/data/icons/beauty-and-cosmetics-5/64/Highlight-bronzer-cosmetic-beauty-makeup-512.png'
+							alt='icon'
+						/>
 						<div>Bronzer</div>
 					</Link>
 					<Link to='/eyebrows'>
+						<img
+							src='https://cdn1.iconfinder.com/data/icons/eyebrow-tattoo-glyph-silhouettes/300/233017476Untitled-3-512.png'
+							alt='icon'
+						/>
 						<div>Eyebrows</div>
 					</Link>
 					<Link to='/eyeshadow'>
+						<img
+							src='https://cdn1.iconfinder.com/data/icons/cosmetic-and-beauty-filled/64/cosmetic-21-512.png'
+							alt='icon'
+						/>
 						<div>Eyeshadow</div>
 					</Link>
 					<Link to='/eyeliner'>
+						<img
+							src='https://cdn2.iconfinder.com/data/icons/beauty-products/100/04-512.png'
+							alt='icon'
+						/>
 						<div>Eyeliner</div>
 					</Link>
 					<Link to='/mascara'>
+						<img
+							src='https://cdn1.iconfinder.com/data/icons/cosmetics-beauty-salon-barbershop-colored/100/cosmetic-06-512.png'
+							alt='icon'
+						/>
 						<div>Mascara</div>
 					</Link>
 					<Link to='/lipstick'>
+						<img
+							src='https://cdn3.iconfinder.com/data/icons/fashion-flat-icons-vol-2/256/55-512.png'
+							alt='icon'
+						/>
 						<div>Lipstick</div>
 					</Link>
 				</div>
