5a5e2d93f5f41b17258ce3f79ef3aeab909cbd8f
sumi419 <sasgar04@gmail.com>
2019-03-17 21:41:30 -0700

    Shopping wip 🛍💖

---
+++ slay-all-day/src/components/Shopping.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	state = {
+		products: []
+	};
+
+	render() {
+		return <div>Hello World</div>;
+	}
+}
+
+export default withRouter(App);
