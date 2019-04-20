51270b2d462237476d8e0b96da5960f02897861d
sumi419 <sasgar04@gmail.com>
2019-04-19 18:46:34 -0700

    Clean up App.js 💕

--- slay-all-day/src/App.js
+++ slay-all-day/src/App.js
@@ -17,171 +17,171 @@ import Footer from './components/ProductType/Footer';
 import data from './data';
 
 class App extends Component {
-	state = {
-		products: [],
-		searchInput: '',
-		filteredSearch: []
-	};
+  state = {
+    products: [],
+    searchInput: '',
+    filteredSearch: []
+  };
 
-	componentDidMount() {
-		// filter out null values before I set them to state
-		let products = data.filter(function(e) {
-			return e.brand === 0 || e.brand;
-		});
-		this.setState({
-			products: products
-		});
-	}
+  componentDidMount() {
+    // filter out null values before I set them to state
+    let products = data.filter(function(e) {
+      return e.brand === 0 || e.brand;
+    });
+    this.setState({
+      products: products
+    });
+  }
 
-	handleSearchInput = (e) => {
-		this.setState({
-			searchInput: e.target.value
-		});
-	};
+  handleSearchInput = (e) => {
+    this.setState({
+      searchInput: e.target.value
+    });
+  };
 
-	handleClick = (e, product) => {
-		e.preventDefault();
-		console.log(product);
-		console.log(e.target);
-	};
+  handleClick = (e, product) => {
+    e.preventDefault();
+    console.log(product);
+    console.log(e.target);
+  };
 
-	render() {
-		return (
-			<div className='App'>
-				<NavBar
-					searchInput={this.state.searchInput}
-					filteredSearch={this.state.filteredSearch}
-					handleSearchInput={this.handleSearchInput}
-				/>
-				{/*	
-					Ternary Operator:
-						a = 3
-						b = 2
-						if ( (a + b)  == 5 
-							do something 
-						else 
-							do something else
+  render() {
+    return (
+      <div className='App'>
+        <NavBar
+          searchInput={this.state.searchInput}
+          filteredSearch={this.state.filteredSearch}
+          handleSearchInput={this.handleSearchInput}
+        />
+        {/*	
+          Ternary Operator:
+            a = 3
+            b = 2
+            if ( (a + b)  == 5 
+              do something 
+            else 
+              do something else
 
-					If logic has something to do, or do something else,
-					ternary shorthand can be rewritten as evaluate expression,
-					do something, and if not true, do something else
-					(a+b == 5) ? do something : do something else
+          If logic has something to do, or do something else,
+          ternary shorthand can be rewritten as evaluate expression,
+          do something, and if not true, do something else
+          (a+b == 5) ? do something : do something else
 
-					
-					{this.state.products ? (
-					<Route exact path='/' component={Loading} />
-				) : (
-					<Route exact path='/' component={Home} />
-				)}
-				*/}
-				<Route
-					path='/products/:id'
-					render={(props) => <Product products={this.state.products} {...props} />}
-				/>
-				<Route
-					exact
-					path='/'
-					render={(props) => (
-						<Home
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput.toLowerCase()}
-							handleClick={this.handleClick}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/foundation'
-					render={(props) => (
-						<Foundation
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/blush'
-					render={(props) => (
-						<Blush
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/bronzer'
-					render={(props) => (
-						<Bronzer
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/eyebrows'
-					render={(props) => (
-						<Eyebrows
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/eyeshadow'
-					render={(props) => (
-						<Eyeshadow
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/eyeliner'
-					render={(props) => (
-						<Eyeliner
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/mascara'
-					render={(props) => (
-						<Mascara
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/lipstick'
-					render={(props) => (
-						<Lipstick
-							products={this.state.products}
-							filteredSearch={this.state.filteredSearch}
-							searchInput={this.state.searchInput}
-						/>
-					)}
-				/>
-				<Footer />
-			</div>
-		);
-	}
+          
+          {this.state.products ? (
+          <Route exact path='/' component={Loading} />
+        ) : (
+          <Route exact path='/' component={Home} />
+        )}
+        */}
+        <Route
+          path='/products/:id'
+          render={(props) => <Product products={this.state.products} {...props} />}
+        />
+        <Route
+          exact
+          path='/'
+          render={(props) => (
+            <Home
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput.toLowerCase()}
+              handleClick={this.handleClick}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/foundation'
+          render={(props) => (
+            <Foundation
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/blush'
+          render={(props) => (
+            <Blush
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/bronzer'
+          render={(props) => (
+            <Bronzer
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/eyebrows'
+          render={(props) => (
+            <Eyebrows
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/eyeshadow'
+          render={(props) => (
+            <Eyeshadow
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/eyeliner'
+          render={(props) => (
+            <Eyeliner
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/mascara'
+          render={(props) => (
+            <Mascara
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/lipstick'
+          render={(props) => (
+            <Lipstick
+              products={this.state.products}
+              filteredSearch={this.state.filteredSearch}
+              searchInput={this.state.searchInput}
+            />
+          )}
+        />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default withRouter(App);
