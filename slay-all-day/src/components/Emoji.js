f9ea2a7ab9e3bdfd97b7052bb3149ec65869754b
sumi419 <sasgar04@gmail.com>
2025-12-22 20:59:40 -0800

    Successfully deployed to GitHub Pages

--- slay-all-day/src/components/Emoji.js
+++ slay-all-day/src/components/Emoji.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default function Emoji({ label, symbol }) {
-	return (
-		<span
-			className='emoji'
-			role='img'
-			aria-label={label ? label : ''}
-			aria-hidden={label ? 'false' : 'true'}>
-			{symbol}
-		</span>
-	);
+  return (
+    <span
+      className='emoji'
+      role='img'
+      aria-label={label ? label : ''}
+      aria-hidden={label ? 'false' : 'true'}>
+      {symbol}
+    </span>
+  );
 }
