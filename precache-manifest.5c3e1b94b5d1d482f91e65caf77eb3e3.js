0b844884ca2a109005945e4963fb599efe1744ba
sumi419 <sasgar04@gmail.com>
2025-12-22 20:58:10 -0800

    Updates

---
+++ precache-manifest.5c3e1b94b5d1d482f91e65caf77eb3e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b5d76d4767dc1de786c",
+    "url": "/highlight/static/css/main.83631737.chunk.css"
+  },
+  {
+    "revision": "8b5d76d4767dc1de786c",
+    "url": "/highlight/static/js/main.730789b5.chunk.js"
+  },
+  {
+    "revision": "b2b9f76c40cc314a4553",
+    "url": "/highlight/static/js/runtime~main.48a0c4ef.js"
+  },
+  {
+    "revision": "cd10c389e631599471e7",
+    "url": "/highlight/static/js/2.b51ece54.chunk.js"
+  },
+  {
+    "revision": "1f82acd63ddf69980a11ec046781fc71",
+    "url": "/highlight/static/media/makeup2.1f82acd6.jpg"
+  },
+  {
+    "revision": "f7758115bb5025bb77c465163789c449",
+    "url": "/highlight/index.html"
+  }
+];
